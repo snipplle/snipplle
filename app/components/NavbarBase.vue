91555862e24785ea5c733ed4299c08b817f05257
DenisOstr <denisostrovskis1@gmail.com>
2025-09-28 00:15:27 +0300

    feat(navbar): add base navbar component with dynamic title

---
+++ app/components/NavbarBase.vue
@@ -0,0 +1,19 @@
+<template>
+  <ClientOnly>
+    <UDashboardNavbar :title="pageTitle">
+      <template #right>
+        <UButton>Create Snippet</UButton>
+      </template>
+    </UDashboardNavbar>
+  </ClientOnly>
+</template>
+
+<script setup lang="ts">
+  import { capitalize } from 'vue'
+
+  const route = useRoute()
+
+  const pageTitle = computed(() => {
+    return capitalize(route.fullPath.split('/')[3] as string)
+  })
+</script>
