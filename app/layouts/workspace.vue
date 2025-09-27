dbe083ec151c90d242cacff592050149265b8fd6
DenisOstr <denisostrovskis1@gmail.com>
2025-09-28 00:15:33 +0300

    feat(layouts): add workspace layout with sidebar and navbar

---
+++ app/layouts/workspace.vue
@@ -0,0 +1,16 @@
+<template>
+  <UDashboardGroup>
+    <SidebarBase />
+
+    <UDashboardPanel>
+      <template #header>
+        <NavbarBase />
+      </template>
+      
+      <template #body>
+        <slot />
+      </template>
+    </UDashboardPanel>
+  </UDashboardGroup>
+</template>
+
