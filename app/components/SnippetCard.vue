139160c2c9bf1adbd94423e19e9395fc756b3e0e
DenisOstr <denisostrovskis1@gmail.com>
2025-09-28 00:59:28 +0300

    feat(components): add SnippetCard component for displaying code snippets

---
+++ app/components/SnippetCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <ClientOnly>
+    <UCard
+      :ui="{
+        body: 'sm:p-0'
+      }"
+      class="group cursor-pointer"
+      @click="openSnippet"
+    >
+      <div class="w-full p-0 rounded-md">
+        <SandpackProvider
+          :theme="{
+            colors: {
+              surface1: '#1f1f27'
+            }
+          }"
+          class="!rounded-t-md !rounded-b-none"
+        >
+          <SandpackLayout class="max-h-32 text-xs !border-none !rounded-t-md !rounded-b-none">
+            <SandpackCodeViewer :code="code" class="rounded-t-md rounded-b-none" />
+          </SandpackLayout>
+        </SandpackProvider>
+      </div>
+
+      <div class="text-sm p-2.5 space-y-3">
+        <div class="flex items-center justify-between">
+          <h1 class="font-semibold">Snippet name</h1>
+
+          <SnippetPreview :code="code" />
+        </div>
+        
+        <div class="flex items-center justify-between">
+          <div class="space-x-1">
+            <UBadge color="info" variant="subtle" size="sm">TypeScript</UBadge>
+            <UBadge color="warning" variant="subtle" size="sm">String</UBadge>
+            <!-- <UBadge color="primary" variant="subtle" size="sm">Transform</UBadge> -->
+          </div>
+
+          <div class="flex items-center space-x-1">
+            <p class="text-xs">43</p>
+            <UIcon name="i-hugeicons-download-01" class="text-lg" />
+          </div>
+        </div>
+      </div>
+    </UCard>
+  </ClientOnly>
+</template>
+
+<script setup lang="ts">
+  import {
+    SandpackProvider,
+    SandpackLayout,
+    SandpackCodeViewer
+  } from 'sandpack-vue3'
+
+  const code = 'const users: { id: number; name: string }[] = [\n  { id: 1, name: "Alice" },\n  { id: 2, name: "Bob" },\n  { id: 3, name: "Charlie" },\n]\n\nfunction findUser(id: number) {\n  return users.find(u => u.id === id) ?? { id, name: "Unknown" }\n}\n\nconsole.log(findUser(2))'
+
+  function openSnippet() {
+    return navigateTo('/workspace/test/snippet/1')
+  }
+</script>
