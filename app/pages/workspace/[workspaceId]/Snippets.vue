fa6a919de3b4131b1949f1ccc20e5ef0e4fc8ce6
DenisOstr <denisostrovskis1@gmail.com>
2025-09-28 00:59:38 +0300

    feat(workspace): add snippets page with filter and search functionality

---
+++ app/pages/workspace/[workspaceId]/Snippets.vue
@@ -0,0 +1,43 @@
+<template>
+  <ClientOnly>
+    <div class="space-y-4">
+      <div class="flex items-center justify-between">
+        <div class="space-x-1">
+          <UButton
+            icon="i-hugeicons-filter"
+            color="neutral"
+            variant="subtle"
+            size="sm"
+          >
+            Filter
+          </UButton>
+          <UButton
+            icon="i-hugeicons-unfold-more"
+            color="neutral"
+            variant="subtle"
+            size="sm"
+          >
+            Order by
+          </UButton>
+        </div>
+
+        <UInput
+          icon="i-hugeicons-search-01"
+          variant="subtle"
+          size="sm"
+          placeholder="Search..."
+        />
+      </div>
+      
+      <div class="grid grid-cols-4 gap-4">
+        <SnippetCard v-for="i in 6" :key="i" />
+      </div>
+    </div>
+  </ClientOnly>
+</template>
+
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'workspace'
+  })
+</script>
