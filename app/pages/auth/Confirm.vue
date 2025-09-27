49a10491fb95cefa5e2a4921df99a40fe7810070
DenisOstr <denisostrovskis1@gmail.com>
2025-09-27 18:20:09 +0300

    feat(auth): add signin, signup and confirm pages with supabase integration

---
+++ app/pages/auth/Confirm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div/>
+</template>
+
+<script setup lang="ts">
+  const user = useSupabaseUser()
+
+  watch(user, () => {
+    if (user.value) {
+      if (!user.value.user_metadata?.onboardingCompleted) {
+        return navigateTo('/welcome')
+      }
+
+      return navigateTo('/workspace/test/snippets')
+    }
+
+    return navigateTo('/auth/signin')
+  }, { immediate: true })
+</script>
