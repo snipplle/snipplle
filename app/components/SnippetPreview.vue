4baf683b619b92075b0987b1ed07c1c6eab2a809
DenisOstr <denisostrovskis1@gmail.com>
2025-09-28 00:59:33 +0300

    feat(SnippetPreview): add new component for code snippet preview

---
+++ app/components/SnippetPreview.vue
@@ -0,0 +1,43 @@
+<template>
+  <ClientOnly>
+    <UModal>
+      <UButton
+        icon="i-hugeicons-view"
+        class="hidden group-hover:block p-0"
+        color="neutral"
+        variant="link"
+        size="sm"
+        @click.stop
+      />
+
+      <template #content>
+        <div class="p-1">
+          <SandpackProvider
+            :theme="{
+              colors: {
+                surface1: '#1f1f27'
+              }
+            }"
+            class="!rounded-md"
+          >
+            <SandpackLayout class="group min-h-24 text-xs !border-none !rounded-md">
+              <SandpackCodeViewer :code="code" class="rounded-md" />
+            </SandpackLayout>
+          </SandpackProvider>
+        </div>
+      </template>
+    </UModal>
+  </ClientOnly>
+</template>
+
+<script setup lang="ts">
+  import {
+    SandpackProvider,
+    SandpackLayout,
+    SandpackCodeViewer
+  } from 'sandpack-vue3'
+
+  defineProps<{
+    code: string
+  }>()
+</script>
