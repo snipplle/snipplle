87d486dd162bcea74c2581ae99975ae11ba3465e
DenisOstr <denisostrovskis1@gmail.com>
2025-09-28 00:15:22 +0300

    refactor(SidebarBase): simplify sidebar structure and add new navigation items

--- app/components/SidebarBase.vue
+++ app/components/SidebarBase.vue
@@ -1,32 +1,44 @@
 <template>
   <ClientOnly>
-    <UDashboardSidebar collapsible>
-      <template #header="{ collapsed }">
+    <UDashboardSidebar collapsed>
+      <template #header>
         <!-- <Logo v-if="!collapsed" class="h-5 w-auto shrink-0" /> -->
-        <UIcon v-if="!collapsed" name="i-hugeicons-document-code" class="size-5 text-primary mx-auto" />
-        <UIcon v-else name="i-hugeicons-document-code" class="size-5 text-primary mx-auto" />
+        <UIcon name="i-hugeicons-document-code" class="size-5 text-primary mx-auto" />
       </template>
       
-      <template #default="{ collapsed }">
+      <template #default>
         <UNavigationMenu
-          :items="items"
-          :collapsed="collapsed"
+          :items="items[0]"
           orientation="vertical"
           color="neutral"
+          :ui="{
+            list: 'space-y-1.5'
+          }"
+          collapsed
+          tooltip
+        />
+
+        <UNavigationMenu
+          :items="items[1]"
+          orientation="vertical"
+          color="neutral"
+          class="mt-auto"
+          :ui="{
+            list: 'space-y-1.5'
+          }"
+          collapsed
+          tooltip
         />
       </template>
 
       <template #footer>
-        <div class="w-full flex justify-between">
-          <UDashboardSidebarCollapse />
-          <UButton
-            type="button"
-            color="neutral"
-            variant="ghost"
-            icon="i-hugeicons-logout-02"
-            @click="logout"
-          />
-        </div>
+        <UButton
+          type="button"
+          color="neutral"
+          variant="ghost"
+          icon="i-hugeicons-logout-02"
+          @click="logout"
+        />
       </template>
     </UDashboardSidebar>
   </ClientOnly>
@@ -45,6 +57,22 @@
       label: 'Snippets',
       icon: 'i-hugeicons-document-code',
       to: `/workspace/${activeWorkspace}/snippets`
+    }, {
+      label: 'Collections',
+      icon: 'i-hugeicons-code-folder',
+      to: `/workspace/${activeWorkspace}/collections`
+    }, {
+      label: 'Settings',
+      icon: 'i-hugeicons-settings-02',
+      to: `/workspace/${activeWorkspace}/settings`
+    }], [{
+      label: 'Docs',
+      icon: 'i-hugeicons-book-02',
+      target: '_blank'
+    }, {
+      label: 'Github',
+      icon: 'i-hugeicons-github',
+      target: '_blank'
     }]]
   })
 </script>
