d995c1f72000c62106256d19ec95fca530cd4cfe
DenisOstr <denisostrovskis1@gmail.com>
2025-09-28 20:14:40 +0300

    refactor(DownloadSnippet): simplify snippet download UI by removing modal wrapper

--- app/components/DownloadSnippet.vue
+++ app/components/DownloadSnippet.vue
@@ -1,61 +1,50 @@
 <template>
   <ClientOnly>
-    <UModal>
-      <UButton
-        icon="i-hugeicons-download-01"
-        color="neutral"
-        variant="subtle"
-        size="sm"
-      />
+    <div class="p-2 space-y-6">
+      <div class="flex flex-col space-y-2">
+        <p class="text-xs font-semibold">Snippet URL (CLI)</p>
 
-      <template #content>
-        <div class="p-4 space-y-6">
-          <div class="flex flex-col space-y-2">
-            <p class="text-xs font-semibold">Snippet URL (CLI)</p>
+        <div class="bg-neutral-700 w-full p-1 rounded-lg">
+          <div
+            class="bg-neutral-800 w-full p-1.5 flex items-center justify-between rounded-md"
+          >
+            <p class="text-sm">test/snippet-name@latest</p>
 
-            <div class="bg-neutral-700 w-full p-1 rounded-lg">
-              <div
-                class="bg-neutral-800 w-full p-1.5 flex items-center justify-between rounded-md"
-              >
-                <p class="text-sm">test/snippet-name@latest</p>
-
-                <UButton
-                  icon="i-hugeicons-copy-01"
-                  color="neutral"
-                  variant="link"
-                  size="sm"
-                  @click="copySnippetURL('latest')"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div class="space-y-2">
-            <p class="text-xs font-semibold">Available versions</p>
-
-            <div
-              v-for="i in 3"
-              :key="i"
-              class="bg-neutral-700 w-full p-1 rounded-lg"
-            >
-              <div
-                class="bg-neutral-800 w-full px-2 flex items-center justify-between rounded-md"
-              >
-                <p class="text-xs">test/snippet-name@{{ i }}</p>
-
-                <UButton
-                  icon="i-hugeicons-copy-01"
-                  color="neutral"
-                  variant="link"
-                  size="sm"
-                  @click="copySnippetURL(i)"
-                />
-              </div>
-            </div>
+            <UButton
+              icon="i-hugeicons-copy-01"
+              color="neutral"
+              variant="link"
+              size="sm"
+              @click="copySnippetURL('latest')"
+            />
           </div>
         </div>
-      </template>
-    </UModal>
+      </div>
+
+      <div class="space-y-2">
+        <p class="text-xs font-semibold">Available versions</p>
+
+        <div
+          v-for="i in 3"
+          :key="i"
+          class="bg-neutral-700 w-full p-1 rounded-lg"
+        >
+          <div
+            class="bg-neutral-800 w-full px-2 flex items-center justify-between rounded-md"
+          >
+            <p class="text-xs">test/snippet-name@{{ i }}</p>
+
+            <UButton
+              icon="i-hugeicons-copy-01"
+              color="neutral"
+              variant="link"
+              size="sm"
+              @click="copySnippetURL(i)"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   </ClientOnly>
 </template>
 
