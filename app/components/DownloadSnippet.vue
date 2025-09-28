72aef1dc4a049a97a462b57d263ec60ddc562c7c
DenisOstr <denisostrovskis1@gmail.com>
2025-09-28 17:39:48 +0300

    feat(components): add DownloadSnippet modal component for copying snippet URLs

---
+++ app/components/DownloadSnippet.vue
@@ -0,0 +1,68 @@
+<template>
+  <ClientOnly>
+    <UModal>
+      <UButton
+        icon="i-hugeicons-download-01"
+        color="neutral"
+        variant="subtle"
+        size="sm"
+      />
+
+      <template #content>
+        <div class="p-4 space-y-6">
+          <div class="flex flex-col space-y-2">
+            <p class="text-xs font-semibold">Snippet URL (CLI)</p>
+            
+            <div class="bg-neutral-700 w-full p-1 rounded-lg">
+              <div class="bg-neutral-800 w-full p-1.5 flex items-center justify-between rounded-md">
+                <p class="text-sm">test/snippet-name@latest</p>
+
+                <UButton
+                  icon="i-hugeicons-copy-01"
+                  color="neutral"
+                  variant="link"
+                  size="sm"
+                  @click="copySnippetURL('latest')"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div class="space-y-2">
+            <p class="text-xs font-semibold">Available versions</p>
+            
+            <div v-for="i in 3" :key="i" class="bg-neutral-700 w-full p-1 rounded-lg">
+              <div class="bg-neutral-800 w-full px-2 flex items-center justify-between rounded-md">
+                <p class="text-xs">test/snippet-name@{{ i }}</p>
+
+                <UButton
+                  icon="i-hugeicons-copy-01"
+                  color="neutral"
+                  variant="link"
+                  size="sm"
+                  @click="copySnippetURL(i)"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+    </UModal>
+  </ClientOnly>
+</template>
+
+<script setup lang="ts">
+  const { copy } = useClipboard()
+  const toast = useToast()
+
+  function copySnippetURL(version: string | number) {
+    copy(`test/snippet-name@${version}`)
+
+    toast.add({
+      title: 'Success',
+      description: 'Snippet URL copied to clipboard',
+      color: 'success',
+      icon: 'i-hugeicons-checkmark-circle-01'
+    })
+  }
+</script>
