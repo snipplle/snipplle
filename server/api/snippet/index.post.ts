c5cb3660608ce375e46c6a1de4d58dbd3ed1c638
DenisOstr <denisostrovskis1@gmail.com>
2025-09-28 17:40:47 +0300

    feat(snippet): add get and post endpoints for snippet management

---
+++ server/api/snippet/index.post.ts
@@ -0,0 +1,87 @@
+import { serverSupabaseUser, serverSupabaseClient } from '#supabase/server'
+import { createId } from '@paralleldrive/cuid2'
+import slugify from 'slugify'
+
+import type { Database } from '~/types/database.types'
+
+export default defineEventHandler(async (event) => {
+  const { name, description, tags, isPublic, workspaceId } = await readBody(event)
+  const user = await serverSupabaseUser(event)
+  const supabase = await serverSupabaseClient<Database>(event)
+
+  if (!user?.id) {
+    throw createError({
+      statusCode: 401,
+      message: 'Unauthorized',
+    })
+  }
+
+  const tagIds = []
+  
+  for (const tag of tags) {
+    const { data: existTag } = await supabase
+      .from('tags')
+      .select('id')
+      .eq('name', tag.name)
+      .single()
+
+    if (existTag) {
+      continue
+    }
+    
+    const { data, error } = await supabase
+      .from('tags')
+      .insert({
+        id: createId(),
+        name: tag.name,
+        color: tag.color,
+      })
+      .select('id')
+      .single()
+
+    if (error) {
+      throw createError({
+        statusCode: 500,
+        message: error.message,
+      })
+    }
+
+    tagIds.push(data.id)
+  }
+
+  const { data, error } = await supabase
+    .from('snippets')
+    .insert({
+      id: createId(),
+      name,
+      slug: slugify(name, {
+        lower: true,
+        remove: /[*+~.()'"!:@]/g,
+      }),
+      description,
+      is_public: isPublic,
+      created_by: user?.id,
+      workspace_id: workspaceId,
+    })
+    .select()
+    .single()
+
+  if (error) {
+    throw createError({
+      statusCode: 500,
+      message: error.message,
+    })
+  }
+
+  for (const tagId of tagIds) {
+    await supabase
+      .from('snippet_tags')
+      .insert({
+        id: createId(),
+        snippet_id: data.id,
+        tag_id: tagId,
+      })
+  }
+
+  return data
+})
