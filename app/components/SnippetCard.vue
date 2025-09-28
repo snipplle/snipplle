aba0d8318e1d3090d258efab6ddc88bda214c07b
DenisOstr <denisostrovskis1@gmail.com>
2025-09-28 20:14:59 +0300

    feat(SnippetCard): add conditional preview rendering and dynamic data

--- app/components/SnippetCard.vue
+++ app/components/SnippetCard.vue
@@ -9,6 +9,7 @@
     >
       <div class="w-full p-0 rounded-md">
         <SandpackProvider
+          v-if="snippet.preview"
           :theme="{
             colors: {
               surface1: '#1f1f27',
@@ -20,18 +21,28 @@
             class="max-h-32 text-xs !border-none !rounded-t-md !rounded-b-none"
           >
             <SandpackCodeViewer
-              :code="code"
+              :code="snippet.preview"
               class="rounded-t-md rounded-b-none"
             />
           </SandpackLayout>
         </SandpackProvider>
+
+        <div
+          v-else
+          class="min-h-32 bg-[#1f1f27] flex items-center justify-center"
+        >
+          <UIcon
+            name="i-hugeicons-document-code"
+            class="text-[54px] text-neutral-600"
+          />
+        </div>
       </div>
 
       <div class="text-sm p-2.5 space-y-3">
         <div class="flex items-center justify-between">
           <h1 class="font-semibold">{{ snippet.name }}</h1>
 
-          <SnippetPreview :code="code" name="Snippet name" />
+          <SnippetPreview :code="snippet.preview" />
         </div>
 
         <div class="flex items-center justify-between">
@@ -47,7 +58,7 @@
           </div>
 
           <div class="flex items-center space-x-1">
-            <p class="text-xs">43</p>
+            <p class="text-xs">{{ snippet.downloads }}</p>
             <UIcon name="i-hugeicons-download-01" class="text-lg" />
           </div>
         </div>
@@ -69,9 +80,6 @@
   }>()
 
   const globalStore = useGlobalStore()
-
-  const code =
-    'const users: { id: number; name: string }[] = [\n  { id: 1, name: "Alice" },\n  { id: 2, name: "Bob" },\n  { id: 3, name: "Charlie" },\n]\n\nfunction findUser(id: number) {\n  return users.find(u => u.id === id) ?? { id, name: "Unknown" }\n}\n\nconsole.log(findUser(2))'
 
   const tags = computed(() => {
     const tagList = []
