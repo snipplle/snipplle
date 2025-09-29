bbe0c2749dc6ef33bcc0fcfb83d7357837b442a3
DenisOstr <denisostrovskis1@gmail.com>
2025-09-29 22:44:05 +0300

    feat: add CodeEditorListener component to watch code changes

---
+++ app/components/CodeEditorListener.vue
@@ -0,0 +1,15 @@
+<template>
+  <div />
+</template>
+
+<script setup lang="ts">
+  import { useActiveCode } from 'sandpack-vue3'
+
+  const emits = defineEmits(['change'])
+
+  const { code } = useActiveCode()
+
+  watch(code, (newCode) => {
+    emits('change', newCode)
+  })
+</script>
