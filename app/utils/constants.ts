cc99d99c5d0c42526c0f5159dd412376a9689e40
DenisOstr <denisostrovskis1@gmail.com>
2025-10-02 00:01:41 +0300

    feat(constants): add language definitions with colors and labels

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -21,3 +21,62 @@ export const tagColors = [
 
 export const editorHtml =
   '<!DOCTYPE html><html><head><style>html, body { background: #181923 !important; }</style></head><body><div id="root"></div></body></html>'
+
+export const language: Record<string, any> = {
+  ts: {
+    label: 'TypeScript',
+    color: 'blue',
+  },
+  js: {
+    label: 'JavaScript',
+    color: 'yellow',
+  },
+  html: {
+    label: 'HTML',
+    color: 'orange',
+  },
+  css: {
+    label: 'CSS',
+    color: 'green',
+  },
+  json: {
+    label: 'JSON',
+    color: 'emerald',
+  },
+  md: {
+    label: 'Markdown',
+    color: 'red',
+  },
+  xml: {
+    label: 'XML',
+    color: 'violet',
+  },
+  yaml: {
+    label: 'YAML',
+    color: 'violet',
+  },
+  sql: {
+    label: 'SQL',
+    color: 'sky',
+  },
+  php: {
+    label: 'PHP',
+    color: 'blue',
+  },
+  rb: {
+    label: 'Ruby',
+    color: 'red',
+  },
+  py: {
+    label: 'Python',
+    color: 'blue',
+  },
+  go: {
+    label: 'Go',
+    color: 'cyan',
+  },
+  rs: {
+    label: 'Rust',
+    color: 'amber',
+  },
+}
