20ccb612d1efe5947115bf0f129ed3ecbe64a94c
DenisOstr <denisostrovskis1@gmail.com>
2025-10-04 14:25:55 +0300

    refactor(Snippets.vue): simplify layout and add empty state

--- app/pages/workspace/[workspaceId]/Snippets.vue
+++ app/pages/workspace/[workspaceId]/Snippets.vue
@@ -1,40 +1,30 @@
 <template>
   <ClientOnly>
-    <div class="space-y-4">
-      <div class="flex items-center justify-between">
-        <div class="space-x-1">
-          <UButton
-            icon="i-hugeicons-filter"
-            color="neutral"
-            variant="subtle"
-            size="sm"
-          >
-            Filter
-          </UButton>
-          <UButton
-            icon="i-hugeicons-unfold-more"
-            color="neutral"
-            variant="subtle"
-            size="sm"
-          >
-            Order by
-          </UButton>
-        </div>
-
-        <UInput
-          icon="i-hugeicons-search-01"
-          variant="subtle"
-          size="sm"
-          placeholder="Search..."
-        />
-      </div>
-
-      <div class="grid grid-cols-4 gap-4">
+    <div class="h-full space-y-4">
+      <div v-if="snippets?.length" class="grid grid-cols-4 gap-4">
         <SnippetCard
           v-for="snippet in snippets"
           :key="snippet.id"
           :snippet="snippet"
         />
+      </div>
+
+      <div
+        v-if="!snippets?.length"
+        class="h-full flex flex-col items-center justify-center space-y-2"
+      >
+        <img src="assets/images/Dev.svg" />
+
+        <div class="flex flex-col items-center space-y-4">
+          <div class="flex flex-col items-center">
+            <h1 class="font-semibold">No snippets found</h1>
+            <p class="text-sm text-neutral-400">
+              Create a new snippet to get started.
+            </p>
+          </div>
+
+          <UButton color="primary" variant="subtle">Create snippet</UButton>
+        </div>
       </div>
     </div>
   </ClientOnly>
@@ -42,7 +32,7 @@
 
 <script setup lang="ts">
   definePageMeta({
-    layout: 'workspace',
+    layout: 'library',
   })
 
   const { data: snippets } = await useFetch('/api/snippet', {
