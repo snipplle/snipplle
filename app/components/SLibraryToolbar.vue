6d7fa0058c1c8819fc4bb0ccc2d66d847b6f5f01
DenisOstr <denisostrovskis1@gmail.com>
2025-10-04 14:25:41 +0300

    feat(components): add SLibraryToolbar component with filter and search functionality

---
+++ app/components/SLibraryToolbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <ClientOnly>
+    <UDashboardToolbar>
+      <div class="w-full flex items-center justify-between">
+        <div class="space-x-1">
+          <UButton
+            icon="i-hugeicons-filter"
+            color="neutral"
+            variant="subtle"
+            size="sm"
+          >
+            Filter
+          </UButton>
+          <UButton
+            icon="i-hugeicons-unfold-more"
+            color="neutral"
+            variant="subtle"
+            size="sm"
+          >
+            Order by
+          </UButton>
+        </div>
+
+        <UInput
+          icon="i-hugeicons-search-01"
+          variant="subtle"
+          size="sm"
+          placeholder="Search..."
+        />
+      </div>
+    </UDashboardToolbar>
+  </ClientOnly>
+</template>
