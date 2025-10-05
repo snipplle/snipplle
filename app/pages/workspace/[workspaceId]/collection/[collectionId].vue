524e10f5482c79ce94ce0f6fd9ce1fd85e519ec7
DenisOstr <denisostrovskis1@gmail.com>
2025-10-05 23:25:00 +0300

    feat(collection): add collection details page with layout and api endpoint

--- app/pages/workspace/[workspaceId]/collection/[collectionId].vue
+++ app/pages/workspace/[workspaceId]/collection/[collectionId].vue
@@ -1,19 +1,52 @@
 <template>
   <ClientOnly>
-    <NuxtLayout name="workspace">
-      <div></div>
+    <NuxtLayout name="editor" :title="collection?.name">
+      <div class="flex h-full space-x-2">
+        <div class="flex flex-col w-full space-y-2">
+          <div
+            class="bg-default border border-light-gray-800 w-full h-full p-2 rounded-lg space-y-2"
+          >
+            <div
+              v-for="i in 4"
+              :key="i"
+              class="bg-light-gray-800 border border-light-gray-700 p-4 rounded-md"
+            >
+              {{ i }}
+            </div>
+          </div>
+          <div
+            class="bg-default border border-light-gray-800 w-full h-full p-2 rounded-lg space-y-2"
+          >
+            <div
+              v-for="i in 2"
+              :key="i"
+              class="bg-light-gray-800 border border-light-gray-700 p-4 rounded-md"
+            >
+              {{ i }}
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="bg-default border border-light-gray-800 w-full h-full rounded-lg"
+        ></div>
+      </div>
     </NuxtLayout>
   </ClientOnly>
 </template>
 
 <script setup lang="ts">
   const { params } = useRoute()
+  const globalStore = useGlobalStore()
   const { listen } = useToolbarEvent()
 
-  // const { data: snippet, refresh } = await useFetch<any>(
-  //   `/api/snippet/${params.snippetId}?workspaceId=${globalStore.activeWorkspace?.id}`,
-  //   {
-  //     method: 'get',
-  //   },
-  // )
+  const { data: collection, refresh } = await useFetch<any>(
+    `/api/collection/${params.collectionId}`,
+    {
+      method: 'get',
+      query: {
+        workspaceId: globalStore.activeWorkspace?.id,
+      },
+    },
+  )
 </script>
---
+++ server/api/collection/[id].get.ts
@@ -0,0 +1,33 @@
+import { serverSupabaseUser, serverSupabaseClient } from '#supabase/server'
+
+import type { Database } from '~~/server/types/database.types'
+
+export default defineEventHandler(async (event) => {
+  const { id: slug } = await getRouterParams(event)
+  const { workspaceId } = getQuery(event)
+  const user = await serverSupabaseUser(event)
+  const supabase = await serverSupabaseClient<Database>(event)
+
+  if (!user) {
+    throw createError({
+      statusCode: 401,
+      message: 'Unauthorized',
+    })
+  }
+
+  const { data, error } = await supabase
+    .from('collections')
+    .select('*, collection_versions(id, version, is_latest, path)')
+    .eq('slug', slug)
+    .eq('workspace_id', workspaceId as string)
+    .single()
+
+  if (error) {
+    throw createError({
+      statusCode: 400,
+      message: error.message,
+    })
+  }
+
+  return data
+})
