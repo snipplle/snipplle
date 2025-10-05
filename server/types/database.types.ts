3fab3b41ff7fe8474a25e717fa5f2db4ac8414d1
DenisOstr <denisostrovskis1@gmail.com>
2025-10-05 23:24:15 +0300

    feat(database): add is_latest field to database types

--- server/types/database.types.ts
+++ server/types/database.types.ts
@@ -118,6 +118,7 @@ export type Database = {
           created_at: string
           files: Json
           id: string
+          is_latest: boolean
           path: string
           version: string
         }
@@ -126,6 +127,7 @@ export type Database = {
           created_at?: string
           files: Json
           id: string
+          is_latest?: boolean
           path: string
           version: string
         }
@@ -134,6 +136,7 @@ export type Database = {
           created_at?: string
           files?: Json
           id?: string
+          is_latest?: boolean
           path?: string
           version?: string
         }
