d147556b0440bb6367648fcf77bf591ec09d6577
DenisOstr <denisostrovskis1@gmail.com>
2025-10-05 22:31:10 +0300

    feat(collection): wrap collection page in workspace layout and clientonly

--- app/pages/workspace/[workspaceId]/collection/[collectionId].vue
+++ app/pages/workspace/[workspaceId]/collection/[collectionId].vue
@@ -1,3 +1,19 @@
 <template>
-  <div></div>
+  <ClientOnly>
+    <NuxtLayout name="workspace">
+      <div></div>
+    </NuxtLayout>
+  </ClientOnly>
 </template>
+
+<script setup lang="ts">
+  const { params } = useRoute()
+  const { listen } = useToolbarEvent()
+
+  // const { data: snippet, refresh } = await useFetch<any>(
+  //   `/api/snippet/${params.snippetId}?workspaceId=${globalStore.activeWorkspace?.id}`,
+  //   {
+  //     method: 'get',
+  //   },
+  // )
+</script>
