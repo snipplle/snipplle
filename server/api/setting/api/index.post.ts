991ae9b9e0848c802e7e18cb5610d3966ac6ed39
DenisOstr <denisostrovskis1@gmail.com>
2025-10-18 01:14:29 +0300

    feat(api): add endpoint to create api tokens for workspaces

---
+++ server/api/setting/api/index.post.ts
@@ -0,0 +1,41 @@
+import { serverSupabaseUser, serverSupabaseClient } from '#supabase/server'
+import { createId } from '@paralleldrive/cuid2'
+import { randomBytes } from 'crypto'
+
+import type { Database } from '~~/server/types/database.types'
+
+export default defineEventHandler(async (event) => {
+  const { workspaceId, name } = await readBody(event)
+  const user = await serverSupabaseUser(event)
+  const supabase = await serverSupabaseClient<Database>(event)
+
+  if (!user) {
+    throw createError({
+      statusCode: 401,
+      message: 'Unauthorized',
+    })
+  }
+
+  const rawKey = randomBytes(32).toString('hex')
+
+  const { error } = await supabase
+    .from('api_tokens')
+    .insert({
+      id: createId(),
+      user_id: user.id,
+      workspace_id: workspaceId,
+      name,
+      token: rawKey,
+    })
+    .select()
+    .single()
+
+  if (error) {
+    throw createError({
+      statusCode: 500,
+      message: error.message,
+    })
+  }
+
+  return rawKey
+})
