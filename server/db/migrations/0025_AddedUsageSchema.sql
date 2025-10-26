b2082a60353caca9e221cd724209c617a5546489
DenisOstr <denisostrovskis1@gmail.com>
2025-10-26 16:42:46 +0200

    fix(db): create usages table and remove snippet versions migration

--- server/db/migrations/0025_AddedUsageSchema.sql
+++ server/db/migrations/0025_AddedUsageSchema.sql
@@ -1,1 +1,14 @@
--- Custom SQL migration file, put your code below! --
+CREATE TABLE "usages" (
+	"id" text PRIMARY KEY NOT NULL,
+	"user_id" text,
+	"snippets" integer DEFAULT 0 NOT NULL,
+	"snippet_versions" integer DEFAULT 0 NOT NULL,
+	"collections" integer DEFAULT 0 NOT NULL,
+	"team_members" integer DEFAULT 0 NOT NULL,
+	"ai_requests" integer DEFAULT 0 NOT NULL,
+	"ai_tokens" integer DEFAULT 0 NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "usages" ADD CONSTRAINT "usages_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
