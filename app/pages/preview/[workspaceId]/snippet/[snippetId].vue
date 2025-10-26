4eebbb98e4438c780dde9518a8b1f315a2f392e1
DenisOstr <denisostrovskis1@gmail.com>
2025-10-26 21:13:39 +0200

    feat(preview): add CodeViewer component to display snippet content

--- app/pages/preview/[workspaceId]/snippet/[snippetId].vue
+++ app/pages/preview/[workspaceId]/snippet/[snippetId].vue
@@ -2,6 +2,7 @@
   <ClientOnly>
     <NuxtLayout name="preview" :title="data.name">
       <div class="h-full">
+        <CodeViewer :content="snippetCode" :extensions="extensions" />
         <!-- <Codemirror
           v-model="snippetCode"
           class="editor"
@@ -18,13 +19,41 @@
 </template>
 
 <script setup lang="ts">
+  import { catppuccinMocha } from '@catppuccin/codemirror'
+
   const { params } = useRoute()
   const globalStore = useGlobalStore()
+  const { beautifyCode } = useCodeFormat()
 
-  const { data } = await useFetch(
-    `/api/snippet/${params.snippetId}?workspaceId=${globalStore.previewWorkspaceId}`,
+  const { data } = await useFetch(`/api/snippet/${params.snippetId}`, {
+    method: 'GET',
+    query: {
+      workspaceId: globalStore.previewWorkspaceId,
+    },
+  })
+
+  let extensions = [catppuccinMocha, languages[data.value.language || 'js']]
+  const snippetCode = ref('')
+
+  watch(
+    () => data.value?.snippet_file,
+    async (fileUrl) => {
+      if (!fileUrl) {
+        snippetCode.value = ''
+        extensions = [...extensions, languages[data.value.language || 'js']]
+
+        return
+      }
+
+      const response = await fetch(fileUrl)
+      const code = await response.text()
+
+      snippetCode.value = beautifyCode(code)
+      extensions = [...extensions, languages[data.value.language || 'js']]
+    },
     {
-      method: 'GET',
+      deep: true,
+      immediate: true,
     },
   )
 </script>
