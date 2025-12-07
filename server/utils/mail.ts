6ddf44b7809dbbe1b45d4b9c6f16ce84051db774
DenisOstr <denisostrovskis1@gmail.com>
2025-12-07 22:11:08 +0200

    fix(mail): cast MAIL_PORT to number to ensure type safety

--- server/utils/mail.ts
+++ server/utils/mail.ts
@@ -1,13 +1,14 @@
 import nodemailer from 'nodemailer'
+import type SMTPTransport from 'nodemailer/lib/smtp-transport'
 
 const runtimeConfig = useRuntimeConfig()
 
 export const transporter = nodemailer.createTransport({
   host: runtimeConfig.MAIL_HOST,
-  port: runtimeConfig.MAIL_PORT,
+  port: Number(runtimeConfig.MAIL_PORT),
   secure: true,
   auth: {
     user: runtimeConfig.MAIL_USER,
     pass: runtimeConfig.MAIL_PASSWORD,
   },
-})
+} satisfies SMTPTransport.Options)
